93f68b589363af040be886fc9d6ac4b704f64508
Daniel <159645278+manawarah@users.noreply.github.com>
2024-11-04 22:34:00 -0300

    Delete Projeto MeuSite.Com Main/Projeto MeuSite.Com Main/sit directory

--- Projeto MeuSite.Com Main/Projeto MeuSite.Com Main/sit/pages/components/conteudo.php
+++
@@ -1,4 +0,0 @@
-<div class="noticia-container">
-    <img src="https://medias.revistaoeste.com/wp-content/uploads/2024/07/meu-imposto-favorito-capa.jpg" class="noticia-imagem">
-    <a href="" class="noticia-titulo">Título da Notícia</a>
-</div>
--- Projeto MeuSite.Com Main/Projeto MeuSite.Com Main/sit/pages/components/topo.php
+++
@@ -1,6 +0,0 @@
-<header>
-    <a href="?">Início</a>
-    <a href="?pg=politica">Política</a>
-    <a href="?pg=futebol">Futebol</a>
-    <a href="?pg=emalta">Em Alta</a>
-</header>
